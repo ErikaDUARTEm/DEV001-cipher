9585f32cd8af287a0448648567f4f66270f4dcbf
Erika <erika.duarte.mora@gmail.com>
2022-10-06 17:16:01 -0500

    agregue la parte funcional de encode y resultado

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,25 @@
 //Aquí deben ir los objetos cipher encode y decode
 
-
-
 const cipher = {
-  
+    encode(numeroD, textOriginal){
+        let cadenaAbc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let textMayus = textOriginal.split("");
+        let textoCifrado = "";
+        let ASCII = 0;
+           for(let i= 0; i < textMayus.length; i++) {
+              if(cadenaAbc.indexOf(textMayus[i]) != -1) {
+               ASCII = ((textMayus[i].charCodeAt() - 65 + numeroD) %26 + 65);
+                textoCifrado += String.fromCharCode(ASCII);
+              } else {
+                textoCifrado += textMayus[i];
+            }   
+          } 
+          return textoCifrado; 
+  },
+
+   decode(numeroD, textOriginal){
+
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-
 import cipher from './cipher.js';
 const S= (selector) => document.querySelector(selector);
+
+
 // usuario registrado
 let usuarios = {
   name:"Erika Duarte",
-  password:123456
+  password:"123456"
  }  
  
-
  //borrar usuario y contraseña
 const borrar = () => {
   S(".btn-borrar").addEventListener("click", (e) =>{
@@ -18,20 +18,37 @@ const borrar = () => {
 // validar usuario para iniciar sesion
 const validarUsuario = () =>{
   
-  let usuario = document.getElementById("usuario").value;
-  let password = document.getElementById("password").value;
-    if (usuario === "") return alert("Debes ingresar tu nombre de usuario");  
-    if (password === "") return alert("Debes ingresar tu contraseña");
-    if (usuario === "Erika Duarte" && password === "123456") {
+  let usuario = document.getElementById("usuario");
+  let password = document.getElementById("password");
+    if (usuario.value === "") return alert("Debes ingresar tu nombre de usuario");  
+    if (password.value === "") return alert("Debes ingresar tu contraseña");
+    if (usuarios.name === "Erika Duarte" && usuarios.password === "123456") {
           S(".iniciar").classList.add("ocultar");
           S(".iniciar").classList.remove("mostrar");
           S(".principal").classList.add("mostrar");
           S(".principal").classList.remove("ocultar");
-          S(".usuario").innerHTML= usuario;
+          S(".usuario").innerHTML= usuarios.name;
   } else{
     alert("El usuario y contraseña son incorrectos");
   }
-}
+} 
+
 S(".btn-iniciar").addEventListener("click", validarUsuario);
 
-let abc ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//capturar numero de desplazamiento y textarea
+//letra en valor ASCII
+
+
+   S(".btn-cifrar").addEventListener("click", () => {
+   let numeroD= S(".offset").valueAsNumber;
+   let textOriginal =  S("#entrada").value.toUpperCase();
+   let resultado = cipher.encode(numeroD,textOriginal); 
+   console.log(resultado);
+   document.getElementById("entrada").classList.remove("ver");
+   document.getElementById("entrada").classList.add("nover");
+   document.getElementById("resultado").classList.remove("nover");
+   document.getElementById("resultado").classList.add("ver");
+   S(".resultado").innerHTML = resultado;
+  });
+  
+  
